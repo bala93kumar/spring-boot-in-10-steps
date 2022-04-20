42c0dbf8184eb0fd82d3e04a64b61ab5bb028354
Balakumar <bala93kumar@gmail.com>
2022-04-20 09:15:27 +0530

    first commit

---
+++ src/main/java/com/springboot/basic/springbootin10steps/BookController.java
@@ -0,0 +1,19 @@
+package com.springboot.basic.springbootin10steps;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+	
+	@GetMapping("/books")
+	public List<Book> getAllBooks(){
+		
+		return  List.of(new Book(1,"dsa","bala"));  
+		
+	}
+
+}
